2c70ef7677e61845c52d2af488c5bd4fc91bf6e6
sheep <samcoxe0307@gmail.com>
2024-10-15 09:01:58 +0700

    first commit

--- src/CRUD.java
+++
@@ -1,5 +0,0 @@
-public class CRUD {
-    public static void main(String[] args) {
-        System.out.println("CRUD");
-    }
-}
--- src/TestJdbc.java
+++ src/TestJdbc.java
@@ -1,21 +1,2 @@
-import java.sql.Connection;
-import java.sql.DriverManager;
-import java.sql.SQLException;
-
 public class TestJdbc {
-    private static final String urlConnection = "jdbc:mysql://localhost:3306/btjdbc2";
-
-    private String username = "root";
-    private String password = "123456";
-
-    public Connection connectionDB() {
-        Connection connection = null;
-        try {
-            connection = DriverManager.getConnection(urlConnection, username, password);
-            System.out.println("connection successful");
-        } catch (SQLException e) {
-            e.printStackTrace();
-        }
-        return connection;
-    }
 }
